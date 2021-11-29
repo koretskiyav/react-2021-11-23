785177aa8a8c699c52903227cb282a2e6cca6d55
vorobev <i.vorobev@hh.ru>
2021-11-29 12:33:09 +0300

    use another trick for key generation in Rate component

--- src/components/rate.js
+++ src/components/rate.js
@@ -4,11 +4,9 @@ import { ReactComponent as Star } from '../icons/star.svg';
 export default ({ rating }) => {
   return (
     <span>
-      {Array(rating)
-        .fill(undefined)
-        .map((ignore, index) => (
-          <Star key={index} />
-        ))}
+      {[...Array(rating).keys()].map((key) => (
+        <Star key={key} />
+      ))}
     </span>
   );
 };
