f917ff3ca4941f216802d535f283d9d76aa8a270
Victoriya <vikbelec111@yandex.ru>
2021-11-30 23:16:09 +0300

    feat: add reviews style

--- src/components/review.js
+++ src/components/review.js
@@ -1,9 +1,10 @@
-import Rate from "./rate"
+import Rate from "./rate";
+import styles from './review.module.css';
 
 export default function Review ({user, text, rating}) {
     return (
-        <div>
-            <p>{user}</p>
+        <div className={styles.review}>
+            <h3>{user}</h3>
             <p>{text}</p>
             <Rate value={rating} />
         </div>        
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -4,11 +4,8 @@ export default function Reviews ({reviews}) {
     return (
         <div>
             {reviews.map((review) => {
-                console.log(review.user)
                 return (
-                    <div>
-                        <Review key={review.id} user={review.user} text={review.text} rating={review.rating} />
-                    </div>  
+                    <Review key={review.id} user={review.user} text={review.text} rating={review.rating} />                    
                 )             
             })}
         </div>
