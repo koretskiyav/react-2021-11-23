a6d9efa93ff86f9d0fd547e3d268496e9dba9f03
mkovalev <marina.kovaleva@t-systems.com>
2021-12-07 01:00:14 +0300

    Add PropTypes for banner, button, rate, restaurant, restaurants, reviews, tabs

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import Review from './review';
 import styles from './reviews.module.css';
 
@@ -9,6 +11,17 @@ const Reviews = ({ reviews }) => {
       ))}
     </div>
   );
+};
+
+Review.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      user: PropTypes.string,
+      text: PropTypes.string,
+      rating: PropTypes.number.isRequired,
+    }).isRequired
+  ),
 };
 
 export default Reviews;
