f2fddb358e50fcd092f3c252846d9697032b614b
vorobev <i.vorobev@hh.ru>
2021-12-12 13:12:33 +0300

    add users reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -3,10 +3,12 @@ import order from './order';
 import restaurants from './restaurants';
 import products from './products';
 import reviews from './reviews';
+import users from './reviews';
 
 export default combineReducers({
   order,
   restaurants,
   products,
   reviews,
+  users,
 });
