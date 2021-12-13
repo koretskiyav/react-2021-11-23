b28c1913fbafc840a65a86512b603fece692d1fb
nilenko <nikita.ilenko@t-systems.com>
2021-12-13 22:51:10 +0300

    hw 4

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,14 +1,20 @@
-import PropTypes from 'prop-types';
+import {
+  reviewRatingSelector,
+  reviewTextSelector,
+  reviewUserNameSelector,
+} from '../../../redux/selectors';
 
+import PropTypes from 'prop-types';
 import Rate from '../../rate';
+import { connect } from 'react-redux';
 import styles from './review.module.css';
 
-const Review = ({ user, text, rating }) => (
+const Review = ({ id, userName, text, rating }) => (
   <div className={styles.review} data-id="review">
     <div className={styles.content}>
       <div>
         <h4 className={styles.name} data-id="review-user">
-          {user}
+          {userName}
         </h4>
         <p className={styles.comment} data-id="review-text">
           {text}
@@ -22,13 +28,19 @@ const Review = ({ user, text, rating }) => (
 );
 
 Review.propTypes = {
-  user: PropTypes.string,
+  userName: PropTypes.string,
   text: PropTypes.string,
   rating: PropTypes.number.isRequired,
 };
 
 Review.defaultProps = {
-  user: 'Anonymous',
+  userName: 'Anonymous',
 };
 
-export default Review;
+const mapStateToProps = (state, props) => ({
+  userName: reviewUserNameSelector(state, props.id),
+  text: reviewTextSelector(state, props.id),
+  rating: reviewRatingSelector(state, props.id),
+});
+
+export default connect(mapStateToProps)(Review);
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,10 @@
 import { createSelector } from 'reselect';
 
-// const restaurantsSelector = (state) => state.restaurants;
+export const restaurantsSelector = (state) => state.restaurants;
 const productsSelector = (state) => state.products;
 const orderSelector = (state) => state.order;
+const usersSelector = (state) => state.users;
+const reviewsSelector = (state) => state.reviews;
 
 export const orderProductsSelector = createSelector(
   [productsSelector, orderSelector],
@@ -21,4 +23,36 @@ export const totalSelector = createSelector(
   [orderProductsSelector],
   (orderProducts) =>
     orderProducts.reduce((acc, { subtotal }) => acc + subtotal, 0)
+);
+
+export const productSelector = (state, id) => productsSelector(state)[id];
+
+export const amountSelector = (state, id) => orderSelector(state)[id];
+
+export const reviewSelector = (state, id) => reviewsSelector(state)[id];
+
+export const reviewUserNameSelector = (state, id) =>
+  usersSelector(state)[reviewSelector(state, id).userId].name;
+
+export const reviewTextSelector = (state, id) =>
+  reviewsSelector(state)[id].text;
+
+export const reviewRatingSelector = (state, id) =>
+  reviewsSelector(state)[id].rating;
+
+export const tabsSelector = createSelector(
+  [restaurantsSelector],
+  (restaurants) =>
+    Object.values(restaurants).map(({ id, name }) => ({ id, label: name }))
+);
+
+export const averageRatingSelector = createSelector(
+  [reviewsSelector],
+  (reviews) => {
+    const total = Object.values(reviews).reduce(
+      (acc, { rating }) => acc + rating,
+      0
+    );
+    return Math.round(total / Object.keys(reviews).length);
+  }
 );
