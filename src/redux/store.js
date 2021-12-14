beedc1713450a607664e0a42d858f612403748c5
koretskiyav <koretskiyav@gmail.com>
2021-12-14 14:15:02 +0200

    add HW4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import logger from './middleware/logger';
+import generateId from './middleware/generateId';
 
 import reducer from './reducer';
 
-export default createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(logger))
-);
+const enhancer = applyMiddleware(generateId, logger);
+
+export default createStore(reducer, composeWithDevTools(enhancer));
