75bb476b6313877a7de73ea49de678c2b22fe244
koretskiyav <koretskiyav@gmail.com>
2021-12-14 20:53:07 +0200

    move activeId to redux

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -3,4 +3,6 @@ export const DECREMENT = 'DECREMENT';
 export const REMOVE = 'REMOVE';
 export const ADD_REVIEW = 'ADD_REVIEW';
 
+export const CHANGE_RESTAURANT = 'CHANGE_RESTAURANT';
+
 export const LOAD_RESTAURANTS = 'LOAD_RESTAURANTS';
