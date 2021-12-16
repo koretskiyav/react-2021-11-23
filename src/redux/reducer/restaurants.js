eb9bf4c0da3253e5cb49964c004ed1bceeb4592d
Vera Lobacheva <v.lobacheva@hh.ru>
2021-12-16 19:01:22 +0300

    HW-5 Use immer.js

--- src/redux/reducer/restaurants.js
+++ src/redux/reducer/restaurants.js
@@ -22,24 +22,42 @@ export default (state = initialState, action) => {
 
   switch (type) {
     case CHANGE_RESTAURANT:
-      return { ...state, activeId };
+      // return { ...state, activeId };
+      return produce(state, (draft) => {
+        draft.activeId = activeId;
+      });
     case LOAD_RESTAURANTS + REQUEST:
-      return { ...state, loading: true, error: null };
+      // return { ...state, loading: true, error: null };
+      return produce(state, (draft) => {
+        draft.loading = true;
+        draft.error = null;
+      });
     case LOAD_RESTAURANTS + SUCCESS:
-      return {
+      /*return {
         ...state,
         activeId: data[0].id,
         entities: arrToMap(data),
         loading: false,
         loaded: true,
-      };
+      };*/
+      return produce(state, (draft) => {
+        draft.activeId = data[0].id;
+        draft.entities = arrToMap(data);
+        draft.loading = false;
+        draft.loaded = true;
+      });
     case LOAD_RESTAURANTS + FAILURE:
-      return {
+      /*return {
         ...state,
         loading: false,
         loaded: false,
         error,
-      };
+      };*/
+      return produce(state, (draft) => {
+        draft.loading = false;
+        draft.loaded = false;
+        draft.error = error;
+      });
     case ADD_REVIEW:
       return produce(state, (draft) => {
         draft.entities[restId].reviews.push(reviewId);
--- src/redux/reducer/reviews.js
+++ src/redux/reducer/reviews.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 import { ADD_REVIEW } from '../constants';
 import { normalizedReviews } from '../../fixtures';
 import { arrToMap } from '../utils';
@@ -8,10 +10,18 @@ export default (state = arrToMap(normalizedReviews), action) => {
   switch (type) {
     case ADD_REVIEW:
       const { text, rating } = review;
-      return {
+      /* return {
         ...state,
         [reviewId]: { id: reviewId, userId, text, rating },
-      };
+      }; */
+      return produce(state, (draft) => {
+        draft[reviewId] = {
+          id: reviewId,
+          userId,
+          text,
+          rating,
+        };
+      });
     default:
       return state;
   }
